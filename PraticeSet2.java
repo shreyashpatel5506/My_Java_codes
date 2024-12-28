08e7851d48cedd5cf60073a0bda1c7f86a86e0a9
Shreyash patel <shreyashpatel5506@gmail.com>
2024-12-28 19:51:58 +0530

    code with harry ch1 problem3

---
+++ PraticeSet2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class PraticeSet2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.next();
+        System.err.println("Hello " + name + ", have a good day!");
+    }
+}
