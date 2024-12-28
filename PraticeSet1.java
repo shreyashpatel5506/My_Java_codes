8c4ad4c13c0b9f3f0759243f73b7ad020cd6fcf2
Shreyash patel <shreyashpatel5506@gmail.com>
2024-12-28 19:43:39 +0530

    for summing three user-input numbers
    (Code with harray)

---
+++ PraticeSet1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PraticeSet1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a = input.nextInt();
+        System.out.println(x:"Enter a second number");
+        int b = input.nextInt();
+        System.out.println("enter a Third number");
+        int c= input.nextInt();
+        System.out.println("here is sum of three numbers" + (a+b+c));  
+    }
+}
