8c9087984043005695e5250d38a586beca3f99eb
Shreyash patel <shreyashpatel5506@gmail.com>
2024-12-28 21:43:23 +0530

    PaticeSet3 code with harry CGPI calculation based on user input marks

---
+++ PaticeSet5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PaticeSet5 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a marks of maths out of 100: ");
+        int maths= sc.nextInt();
+        System.out.print("Enter a marks of science out of 100: ");
+        int science= sc.nextInt();
+        System.out.print("Enter a marks of Chemistry out of 100: ");
+        int Chemistry= sc.nextInt();
+        System.out.print("Enter a marks of English out of 100: ");
+        int English= sc.nextInt();
+        
+        System.out.println("Cgpi of maths: "+((double)maths/10));
+        System.out.println("Cgpi of science: "+((double)science/10));
+        System.out.println("Cgpi of Chemistry: "+((double)Chemistry/10));
+        System.out.println("Cgpi of English: "+((double)English/10));
+
+        System.out.println("Total cgpi: "+((double)(maths+science+Chemistry+English)/40));
+    }
+}
