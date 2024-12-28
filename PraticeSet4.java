60f359b9d7c6b70ac7cb0eb96d88d4350d9c99d5
Shreyash patel <shreyashpatel5506@gmail.com>
2024-12-28 20:24:32 +0530

    Code with Harry ch1 problem 5

---
+++ PraticeSet4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PraticeSet4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a  for check integer or not: ");
+        String str= sc.next();
+        boolean isNumeric = true;
+        try {
+             Integer.parseInt(str);
+            isNumeric = true;
+        } catch (Exception e) {
+            isNumeric = false;
+        }
+        if (isNumeric) {
+            System.out.println("The input is a number");
+        } else {
+            System.out.println("The input is not a number");
+        }
+        
+    }
+}
