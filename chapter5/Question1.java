b06737644ffaa55db457637e067d9eb74514f73e
Shreyash patel <shreyashpatel5506@gmail.com>
2024-12-29 11:41:54 +0530

    Add pattern printing program Question1

---
+++ chapter5/Question1.java
@@ -0,0 +1,13 @@
+package chapter5;
+
+
+public class Question1 {
+    public static void main(String[] args) {
+        for(int i=0;i<=4;i++){
+            for(int j=4;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
