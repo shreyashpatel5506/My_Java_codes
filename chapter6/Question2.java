cd1c33c10951b985d838f1e632296c3bee083579
Shreyash patel <shreyashpatel5506@gmail.com>
2024-12-29 16:31:21 +0530

    Add user input functionality to search for an element in the array

--- chapter6/Question2.java
+++ chapter6/Question2.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Question2 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a element: ");
+        double element = sc.nextDouble();
+        double nums[] = { 54.5, 34.5, 23.5, 12.5, 45.5 };
+        for (double item : nums) {
+            if (item == element) {
+                System.out.println("Element found in array");
+                return;
+            }
+        }
     }
 }
