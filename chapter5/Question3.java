4fbd12e35b3706524d7cb3d2442dfe55441a8421
Shreyash patel <shreyashpatel5506@gmail.com>
2024-12-29 11:51:06 +0530

    Add multiplication table program for a given number

---
+++ chapter5/Question3.java
@@ -0,0 +1,12 @@
+package chapter5;
+import java.util.Scanner;
+public class Question3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number which you want table:--");
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + n * i);
+        }
+    }
+}
