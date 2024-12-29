3884019ef78c63da3e710e8aa082b9a000e7e022
Shreyash patel <shreyashpatel5506@gmail.com>
2024-12-29 16:22:04 +0530

    Add prompt for user input in array sum program

--- chapter6/Question1.java
+++ chapter6/Question1.java
@@ -6,6 +6,7 @@ public class Question1 {
         int[] arr = new int[5];
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
             arr[i] = sc.nextInt();
             sum += arr[i];
         }
