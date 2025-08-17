563db2dcf50602b986fac9e63e9db46d4bb926a8
Shreyash Patel <160259841+shreyashpatel5506@users.noreply.github.com>
2025-08-17 18:09:44 +0000

    Floud'sTriangle Pattern

---
+++ PATTERNS/FloudsTriangle.java
@@ -0,0 +1,12 @@
+public class FloudsTriangle {
+    public static void main(String[] args) {
+        int count=1;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+}
